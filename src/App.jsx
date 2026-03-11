16766dcf4af11eead150d66052caceed39c06111
Rafaela Tavares <rafaelajtv@outlook.com>
2026-03-10 21:27:30 -0300

    feat: add ticket form and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,55 @@
+import { useState } from 'react'
+
 function App() {
+  const [nome, setNome] = useState("")
+  const [email, setEmail] = useState("")
+  const [assunto, setAssunto] = useState("")
+  const [mensagem, setMensagem] = useState("")
+
+  function abrirTicket() {
+  const novoTicket = {
+    nome: nome,
+    email: email,
+    assunto: assunto,
+    mensagem: mensagem
+  }
+
+  setTickets([...tickets, novoTicket])
+}
+
   return (
     <div>
       <h1>Help Desk</h1>
+      <form>
+        <input 
+          type="text" 
+          placeholder="Seu nome"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
+        <input 
+          type="email" 
+          placeholder="Seu email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input 
+          type="text" 
+          placeholder="Assunto"
+          value={assunto}
+          onChange={(e) => setAssunto(e.target.value)}
+        />
+        <textarea 
+          placeholder="Descreva seu problema"
+          value={mensagem}
+          onChange={(e) => setMensagem(e.target.value)}
+        />
+
+        const [tickets, setTickets] = useState([])
+
+        <button type="button">Abrir Ticket</button>
+      </form>
+      
     </div>
   )
 }
